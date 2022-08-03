4cfd84ed04f5f5f067c79dad77be6e015dc92205
Muhammad Saqib Ilyas <msaqib@gmail.com>
2022-08-03 21:14:42 +0500

    Fixed not running issue in Mac
    Changed the extension to use webNavigation events instead of tab load events. This appears to have fixed the extension not working on our Mac machines.

--- background.js
+++ background.js
@@ -1,18 +1,10 @@
-chrome.tabs.onUpdated.addListener((tabid, tab) => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        const pageu = tabs[0].url;
-        if (pageu && pageu.includes("educative.io")) {
-            const tokens = pageu.split("/");
-            chrome.tabs.sendMessage(tabid, {
+const filter = {
+	url: [ {urlMatches: 'https://www.educative.io/'},]
+}
+chrome.webNavigation.onCompleted.addListener((details) => {
+            chrome.tabs.sendMessage(details.tabId, {
                 type: "NEW",
-                courseId: tokens[4],
-                lessonId: tokens[5]
+                courseId: "test",
+                lessonId: "also"
             })
-        }
-
-    })
-    /*if (tab.url && tab.url.includes("educative.io"))*/ {
-        //const pageu = tab.url.split("/"); // This isn't quite right. It'll split on the / in https://, I think
-
-    }
-})
+        }, filter);
